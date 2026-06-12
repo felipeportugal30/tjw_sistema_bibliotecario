48e0328a5f090d25c66fd2e95869b6f9659b258e
Felipe-Portugal <fsportugal30@gmail.com>
2026-06-12 00:41:22 -0300

    feat: Iniciando projeto do sistema bibliotecario

---
+++ bibliotecario/src/main/java/com/sistema/bibliotecario/BibliotecarioApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.bibliotecario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecarioApplication.class, args);
+	}
+
+}
---
+++ bibliotecario/src/test/java/com/sistema/bibliotecario/BibliotecarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.bibliotecario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
